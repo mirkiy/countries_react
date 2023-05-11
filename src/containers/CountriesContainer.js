45bcdf67e8ccaa7eead9c4acaf3b98199e040c41
mirkiy <mirizachar@gmail.com>
2023-05-11 15:56:44 +0100

    list of all countries

--- src/containers/CountriesContainer.js
+++ src/containers/CountriesContainer.js
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const CountriesContainer = () => {
-  return <h1>Hello hello</h1>;
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  const getCountries = () => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((countriesData) => setCountries(countriesData));
+  };
+
+  console.log(countries);
+
+  const countryList = countries.map((country) => {
+    return <li>{country.name.common}</li>;
+  });
+
+  return (
+    <>
+      <h1>Countries Container</h1>
+      <ul>{countryList}</ul>
+    </>
+  );
 };
 export default CountriesContainer;
